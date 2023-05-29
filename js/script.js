7d3ae572f7f392d57fe4ddf21130c88546c998bf
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-05-29 15:29:26 +0200

    add const name user , distanceUser , age user

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log('ok js')
+
+const nameUser = document.getElementById("name-user").addEventListener("input", () => console.log(document.getElementById("name-user").value));
+
+const distanceUser = document.getElementById("distance-user").addEventListener("change" , () => console.log(document.getElementById("distance-user").value));
+
+const ageUser = document.getElementById("age-user").addEventListener("input", () => console.log(document.getElementById("age-user").value));
+
+
+
